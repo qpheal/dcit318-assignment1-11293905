e1d66d64f4aa49e2aa868abd769f5a75c90fe64d
Adjei Rapheal <radjei119@st.ug.edu.gh>
2025-08-05 02:25:52 +0000

    Added Triangle Type Identifier app

---
+++ TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class TriangleTypeIdentifier
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the lengths of the three sides of the triangle:");
+
+        Console.Write("Side A: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Side B: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Side C: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        // Check if it can form a triangle first
+        if (a + b > c && a + c > b && b + c > a)
+        {
+            if (a == b && b == c)
+            {
+                Console.WriteLine("This is an Equilateral triangle.");
+            }
+            else if (a == b || a == c || b == c)
+            {
+                Console.WriteLine("This is an Isosceles triangle.");
+            }
+            else
+            {
+                Console.WriteLine("This is a Scalene triangle.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("The given sides do not form a valid triangle.");
+        }
+    }
+}
